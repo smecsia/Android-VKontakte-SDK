410f4a5bbd449e96cb5b7456ea27e84cbb2369b4
Ilya Sadykov <smecsia@gmail.com>
2013-04-17 10:55:11 +0400

    mavenize

---
+++ AndroidVkSdkSample/src/main/java/com/perm/kate/api/sample/Constants.java
@@ -0,0 +1,7 @@
+package com.perm.kate.api.sample;
+
+public class Constants {
+    //Этот ID предназначен только для примера. Пожалуйста замение его ID своего приложения.
+    public static String API_ID="2904017";
+
+}
